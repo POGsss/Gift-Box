f047d078f00fe987ee3687997a1298ca7b931e28
POGsss <92162474+POGsss@users.noreply.github.com>
2024-10-05 06:33:40 +0800

    Update JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("load", load());
 
 function load() {
-  const data = document.getElementById("data");
-  data.innerHTML = "Hello World"
+  const greeting = document.getElementById("data");
+  greeting.innerHTML = "Hello World"
 }
